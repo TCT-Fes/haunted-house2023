2058d531eaff46a377695a0c4523d67f6e3cb5f0
ikuta-m <m23ikuta@tokuyama.kosen-ac.jp>
2023-10-27 11:28:22 +0900

    Arduino

--- src/arduino/LED.ino
+++ src/arduino/LED.ino
@@ -1,44 +1,55 @@
-int Trig = 8;
-int Echo = 9;
-int LED = 13;
-int Duration;
-float Distance;
+#include <Arduino.h>  // Arduino IDEを使う場合はコメントアウトする
+
+const uint8_t echoPin   = 9;
+const uint8_t trigPin   = 8;
+const uint8_t LEDPin = 13;
+
+unsigned long duration;  // 超音波の往復時間(μ秒)
+uint16_t distance;       // 対象までの距離
+uint8_t detection_cnt;   // 検出回数用のカウンタ
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(Trig, OUTPUT);
-  pinMode(Echo, INPUT);
-  pinMode(LED, OUTPUT);
-}
+    pinMode(trigPin, OUTPUT);    // TRIGERピンを OUTPUTに設定
+    pinMode(echoPin, INPUT);     // ECHOピンを INPUT に設定
+    pinMode(LEDPin, OUTPUT);  // もちろん LEDPin は OUTPUT
 
+    Serial.begin(9600);                    // 距離を表示するためにシリアルモニタを使う
+    Serial.println("防犯システム開始: ");  // 一度だけ表示
+}
 void loop() {
-  digitalWrite(trigPin, LOW);
+    // ピンの初期化
+    digitalWrite(trigPin, LOW);
     digitalWrite(echoPin, LOW);
     delayMicroseconds(2);
+
+    // 10μ秒間 TRIGERピン を HIGH にして超音波バーストの放出
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
 
+    // ECHOピン が HIGH になってる時間を duration にセット
     duration = pulseIn(echoPin, HIGH);
+    // 距離 = 時間 × 速さ
+    distance = (uint16_t)(duration / 2) * 0.034;  // 音速 340m/秒 = 0.034cm/μ秒
 
-    distance = (uint16_t)(duration / 2) * 0.034;
 
-    Serial.print(Duration);
-    Serial.print(" ditance ");
-    Serial.print(Distance);
+    // 距離の表示
+    Serial.print("Distance: ");
+    Serial.print(distance);
     Serial.println(" cm");
-  }
-  if (Distance < 100.0) {
-    digitalWrite(LED, HIGH);
-  }
-  else {
-    digitalWrite(LED, LOW);
-  }
-  
-  if (detection_cnt >= 5) {
-        digitalWrite(LED, HIGH);
+
+
+    if (distance <= 100) {  // 距離が 1m 以下なら
+        detection_cnt++;    // 検出用のカウンタに 1 足す
     } else {
-        digitalWrite(LED, LOW);
+        detection_cnt = 0;  // それ以外は 0 にセット
     }
-}
+
+    if (detection_cnt >= 10) {
+        analogWrite(LEDPin, 255);
+        delay(5000);
+    } else {
+        analogWrite(LEDPin,LOW);
+
+    }
 }
