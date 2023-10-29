672e2194c0e8d94fe4696b29d7e0843d02be02ad
Daidesuu <ddai.kon@icloud.com>
2023-10-29 23:06:26 +0900

    kosensai

---
+++ src/arduino/kosen_sai_sound.ino
@@ -0,0 +1,53 @@
+const uint8_t trig_pin   = 2;
+const uint8_t echo_pin   = 3;
+const uint8_t buzzer_pin = 4;
+unsigned long zikan;
+uint8_t kyori;
+uint8_t hani = 17;
+uint8_t cnt;
+long randomtone;
+long times = 0;
+
+
+void setup() {
+    pinMode(trig_pin, OUTPUT);
+    pinMode(echo_pin, INPUT);
+    pinMode(buzzer_pin, OUTPUT);
+    Serial.begin(9600);
+    randomSeed(30);
+}
+
+void loop() {
+    digitalWrite(trig_pin, LOW);
+    digitalWrite(echo_pin, LOW);
+    digitalWrite(buzzer_pin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trig_pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig_pin, LOW);
+
+    zikan = pulseIn(echo_pin, HIGH);
+    kyori = (uint16_t)(zikan / 2) * 0.034;
+    Serial.println(kyori);
+    delay(250);
+
+    if (kyori <= hani || kyori >= 150) {
+        cnt++;
+    } else {
+        cnt = 0;
+    }
+
+    if (cnt >= 1) {
+        while (times < 1000) {
+            randomtone = random(250);
+            tone(buzzer_pin, randomtone + 300);
+            delayMicroseconds(5000);
+            times++;
+        }
+        times = 0;
+        cnt = 0;
+        } else{
+          tone(buzzer_pin,0);
+        }
+}
